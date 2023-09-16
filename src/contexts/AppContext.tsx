112c846b1a4de43018cd52afcc5b92098e67d063
plfmoura <plfmoura96@gmail.com>
2023-09-16 02:37:44 -0300

    fix: navbar icon ignores width

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -7,27 +7,30 @@ export const AppContext = createContext({});
 export const AppProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [width, setWidth] = useState<number>(0);
 
-    useEffect(() => {
-        const handleResize = () => {
-          setWidth(window.innerWidth);
-        };
-        window.addEventListener('resize', handleResize)
-    
-        if(width === 0){
-          handleResize()
-        }
-    
-        return () => {
-          window.removeEventListener('resize', handleResize);
-      };
-      }, [width])
-    
-    return (
-        <AppContext.Provider value={{
-            width,
-            setWidth,
-        }}>
-            {children}
-        </AppContext.Provider>
-    )
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+  
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [width])
+
+  useEffect(() => {
+    if(width === 0){
+      handleResize();
+    }
+  }, [width])
+
+  return (
+    <AppContext.Provider value={{
+      width,
+      setWidth,
+    }}>
+      {children}
+    </AppContext.Provider>
+  )
 }
